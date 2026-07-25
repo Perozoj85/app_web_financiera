fa98f896325e9c66e4a694db7304194b955b5766
Jose Perozo <jose.perozo1@gmail.com>
2026-07-25 09:07:06 -0400

    Instalado contador de visitas de Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react' // <- Línea nueva 1
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics /> {/* <- Línea nueva 2 */}
   </React.StrictMode>,
 )
